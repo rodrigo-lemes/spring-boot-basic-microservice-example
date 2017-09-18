eebf25288e2630446d0f7fe9c238e53fb7075ee6
unknown <rodrigo.lemes@gmail.com>
2017-09-17 21:32:49 -0300

    emove any jsf config or development

--- car-register/src/main/java/com/rodrigo/soukef/microservice/controller/CarRegistrationMicroServiceController.java
+++ car-register/src/main/java/com/rodrigo/soukef/microservice/controller/CarRegistrationMicroServiceController.java
@@ -1,19 +1,10 @@
 package com.rodrigo.soukef.microservice.controller;
 
-import java.util.EnumSet;
 import java.util.List;
 
-import javax.faces.webapp.FacesServlet;
-import javax.servlet.DispatcherType;
-
-import org.ocpsoft.rewrite.servlet.RewriteFilter;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.boot.web.servlet.ServletRegistrationBean;
-import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -67,7 +58,7 @@ public class CarRegistrationMicroServiceController {
 	public String findCarByPlate(@RequestBody String licensePlate) {
 
 		String result;
-		
+
 		Car carData = new Car();
 		carData = new Gson().fromJson(licensePlate, carData.getClass());
 		try {
@@ -79,24 +70,4 @@ public class CarRegistrationMicroServiceController {
 
 		return result;
 	}
-	
-	@Bean
-	   public ServletRegistrationBean servletRegistrationBean() {
-        FacesServlet servlet = new FacesServlet();
-        return new ServletRegistrationBean(servlet, "*.jsf");
-    }
-	
-	@Bean
-    public FilterRegistrationBean rewriteFilter() {
-        FilterRegistrationBean rwFilter = new FilterRegistrationBean(new RewriteFilter());
-        rwFilter.setDispatcherTypes(EnumSet.of(DispatcherType.FORWARD, DispatcherType.REQUEST,
-                DispatcherType.ASYNC, DispatcherType.ERROR));
-        rwFilter.addUrlPatterns("/*");
-        return rwFilter;
-    }
-
-	public static void main(String[] args) throws Exception {
-		SpringApplication.run(CarRegistrationMicroServiceController.class, args);
-	}
-
 }
